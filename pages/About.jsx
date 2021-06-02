c6c500cd67845015e1edffae7678c9841618697f
Kruger092 <kruger092@gmail.com>
2021-06-02 16:42:31 +0300

    first commit

---
+++ pages/About.jsx
@@ -0,0 +1,12 @@
+
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">React App</h1>
+            <p className="lead">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            </p>
+        </div>
+    </div>
+)
